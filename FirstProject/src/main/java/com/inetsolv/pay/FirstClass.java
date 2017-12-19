4a05b17b04f2c90969154afd7208e7b50a6a71a6
mac <mac@192.168.8.100>
2017-12-19 19:16:03 +0300

    This is change of java class

--- FirstProject/src/main/java/com/inetsolv/pay/FirstClass.java
+++ FirstProject/src/main/java/com/inetsolv/pay/FirstClass.java
@@ -3,7 +3,7 @@ package com.inetsolv.pay;
 
 public class FirstClass {
 	public static void main(String args[]) {
-		   System.out.println();     
+		   System.out.println("Github push");     
 	}  
 
 }
