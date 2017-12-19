c4ed30a1f2252e27a4f3d9416f2e3d37885b51b4
mac <mac@192.168.8.100>
2017-12-19 19:31:39 +0300

    Second change

--- FirstProject/src/main/java/com/inetsolv/pay/FirstClass.java
+++ FirstProject/src/main/java/com/inetsolv/pay/FirstClass.java
@@ -3,7 +3,8 @@ package com.inetsolv.pay;
 
 public class FirstClass {
 	public static void main(String args[]) {
-		   System.out.println("Github push");     
+		   System.out.println("Github push");  
+		   System.out.println("Second change");
 	}  
 
 }
